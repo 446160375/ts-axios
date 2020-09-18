b8156db081bdef4d6201f1e917c8387505c970f7
殷兴平 <446160375@qq.com>
2020-09-18 17:49:07 +0800

    开始url处理

---
+++ src/types/index.ts
@@ -0,0 +1,25 @@
+// 全局类型配置
+
+// 定义method的字符串字面量类型
+export type Method =
+  | 'get'
+  | 'GET'
+  | 'delete'
+  | 'DELETE'
+  | 'head'
+  | 'HEAD'
+  | 'options'
+  | 'OPTIONS'
+  | 'post'
+  | 'POST'
+  | 'put'
+  | 'PUT'
+  | 'patch'
+  | 'PATCH'
+
+export interface AxiosRequestConfig {
+  url: string
+  method?: Method
+  data?: any
+  params?: any
+}
